d5d7324aa3f6d9d8124ad09cd5b1c8884c1a717e
yousri <129407665+yousriB@users.noreply.github.com>
2025-05-14 13:29:17 +0100

    14/05

--- src/app/components/user/profile/profile.component.ts
+++ src/app/components/user/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../../services/user.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-profile',
@@ -12,7 +13,7 @@ export class ProfileComponent implements OnInit {
   userData: any;
   password: string = "";
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private snackBar: MatSnackBar) {}
 
   ngOnInit(): void {
     this.userService.getCurrentUser(localStorage.getItem('userId')!).subscribe({
@@ -21,6 +22,10 @@ export class ProfileComponent implements OnInit {
       },
       error: (error) => {
         console.error('Failed to get current user:', error);
+        this.snackBar.open('Failed to get current user!', 'Close', {
+          duration: 3000,
+          panelClass: ['error-snackbar']
+        });
       }
     });
   }
@@ -28,9 +33,17 @@ export class ProfileComponent implements OnInit {
   UpdateUser() {
     this.userService.updateUser(this.userData._id, this.userData.name, this.userData.email, this.userData.phoneNumber, this.password).subscribe({
       next: (user) => {
+        this.snackBar.open('User updated successfully!', 'Close', {
+          duration: 3000,
+          panelClass: ['success-snackbar']
+        });
       },
       error: (error) => {
         console.error('Failed to update user:', error);
+        this.snackBar.open('Failed to update user!', 'Close', {
+          duration: 3000,
+          panelClass: ['error-snackbar']
+        });
       }
     });
   }
